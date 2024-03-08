f1ee9ad8c54a6e5eebb316d18b2e6aeeebbab4d3
Rafael <rafaelgermanio@gmail.com>
2024-03-08 18:45:50 -0300

    Requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (sequelize, DataTypes) => {
+  const Category =  sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    name: DataTypes.STRING(255)
+  },
+  {
+    timestamps: false,
+  })
+  return Category
+}
